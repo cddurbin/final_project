4cb7ae5d29eb6db285bcc1139854f7315fd50cb7
Colin Durbin <cddurbin@gmail.com>
2015-03-24 10:34:59 +0000

    Create answer model.

---
+++ db/migrate/20150324103354_create_answers.rb
@@ -0,0 +1,12 @@
+class CreateAnswers < ActiveRecord::Migration
+  def change
+    create_table :answers do |t|
+      t.integer :user_id
+      t.integer :question_id
+      t.text :content
+      t.boolean :accepted, :default => false
+
+      t.timestamps null: false
+    end
+  end
+end
