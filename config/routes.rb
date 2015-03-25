cc9ccb135f09d241c389eb3edfc87cfac37c7384
Colin Durbin <cddurbin@gmail.com>
2015-03-25 00:29:13 +0000

    Add nested, shallow routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,17 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  resources :questions, shallow: true do
+    resources :answers do
+      resources :comments, only: [:index, :new, :create]
+    end
+    resources :comments
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
