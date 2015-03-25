da75f39eb0d6281f48673712611f9b2142a6bb58
Colin Durbin <cddurbin@gmail.com>
2015-03-25 20:17:28 +0000

    Load handlebars and write template for show question view.

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -8,10 +8,22 @@ function request(method, url, data) {
 }
 
 function getQuestion(){
-  questionId = $('.question').attr('value');
-  request("GET", "/questions/" + questionId, null).done(function(response) {
+  var questionId = $('#question-container').attr('value');
+  console.log(questionId);
+  request("GET", '/questions/' + questionId, null).done(function(response) {
     console.log(response);
+    var source = $("#questiontpl").html();
+    var template = Handlebars.compile(source);
+    $("#question-container").html(template(response));
   });
+    // $("#templates").load("assets/views/questions/show_question.mustache.html #template1",function(){
+    // var template = $('#questiontpl').innerHTML;
+    // var output = Mustache.render(template, repsonse);
+    // $("#question-container").html(output);
+    // var template = "<h2> {{ title }} </h2>";
+    // var html = Mustache.to_html(template, response);
+    // $('#question-container').html(html);
+    // };
 };
 
 
