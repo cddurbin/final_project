64c0ce0b9a0aa72893d38d2c07d1b3cc9821bb11
Colin Durbin <cddurbin@gmail.com>
2015-03-25 16:26:24 +0000

    Refactor ajax requests into function.

--- app/assets/javascripts/questions.js
+++ app/assets/javascripts/questions.js
@@ -1,18 +1,36 @@
-function showQuestion() {
-  questionId = $('.question').attr('value');
-  console.log(questionId);
-  $.ajax({
-    type: 'GET',
-    url: '/questions/' + questionId,
-    dataType: 'json'
-  }).done(function(response) {
-    console.log(response.title);
-  
-  });
+function request(method, url, data) {
+  return $.ajax({
+            url: url,
+            method: method,
+            dataType: "json",
+            data: data
+          })
 }
+
+function getQuestion(){
+  questionId = $('.question').attr('value');
+  request("GET", "/questions/" + questionId, null).done(function(response) {
+    console.log(response);
+  });
+};
+
+
+
+// function getQuestion('GET', '/') {
+  
+//   console.log(questionId);
+//   $.ajax({
+//     type: 'GET',
+//     url: '/questions/' + questionId,
+//     dataType: 'json'
+//   }).done(function(response) {
+//     console.log(response.title);
+  
+//   });
+// }
 
 
 $(document).ready(function() {
-  showQuestion();
+  getQuestion();
 
 });
