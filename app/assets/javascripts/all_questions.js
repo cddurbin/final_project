1a7a56d0dad894554244269e83ebc4d8059ffb0c
Colin Durbin <cddurbin@gmail.com>
2015-03-28 17:44:56 +0000

    Add ga log to topbar and remove turbolinks.

--- app/assets/javascripts/all_questions.js
+++ app/assets/javascripts/all_questions.js
@@ -26,5 +26,5 @@ function getAllQuestions(){
 };
 
 $(document).ready(function(){
-  getAllQuestions();
+  // getAllQuestions();
 });
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  # authorize_resource
+  
+  def show
+    @user = User.find(params[:id])
+  end
+end
