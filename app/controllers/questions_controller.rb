27072778ced594314fb454d21f667e382846787c
Colin Durbin <cddurbin@gmail.com>
2015-04-01 16:41:43 +0100

    Add tags to question json request.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,7 +19,7 @@ class QuestionsController < ApplicationController
     
     respond_to do |format|
       format.html
-      format.json { render json: @question.as_json(include: { user: {}, votes: {},comments: {include: [:user]}})}
+      format.json { render json: @question.as_json(include: { tags: {}, user: {}, votes: {},comments: {include: [:user]}})}
       # format.json { render json: @question.as_json(include: { user: {}, comments: {include: [:user]}, answers: {include: [:comments ]}, votes: {}, watches: {}, watchers: {} })}
     end
 
