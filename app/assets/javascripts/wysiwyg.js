514c646255b8e7c94a1dffe8b857017d1c12c570
Colin Durbin <cddurbin@gmail.com>
2015-04-10 12:14:07 +0100

    Build editor js functions.

--- app/assets/javascripts/wysiwyg.js
+++ app/assets/javascripts/wysiwyg.js
@@ -1,3 +1,61 @@
+function iFrameOn () {
+  // $('#richTextField').contents().get(0).designMode = 'On';
+  document.getElementById('#richTextField').contentDocument.designMode = 'On';
+}
+
+function editorBold () {
+  $('#richTextField').document.execCommand('bold', false, null);
+}
+
+function editorUnderline () {
+  $('#richTextField').document.execCommand('underline', false, null);
+}
+
+function editorItalic () {
+  $('#richTextField').document.execCommand('italic', false, null);
+}
+
+function editorTextSize () {
+  var size = prompt('Enter a size 1-7', '');
+  $('#richTextField').document.execCommand('fontSize', false, size);
+}
+
+function editorTextColor () {
+  var color = prompt('Provide a color', '');
+  $('#richTextField').document.execCommand('foreColor', false, color);
+}
+
+function editorLink () {
+  var linkUrl = prompt('Enter a link', 'http://')
+  $('#richTextField').document.execCommand('CreateLink', false, linkUrl);
+}
+
+function editorUnlink () {
+  $('#richTextField').document.execCommand('Unlink', false, null);
+}
+
+function editorHorizontalRule () {
+  $('#richTextField').document.execCommand('insertHorizontalRule', false, null);
+}
+
+function editorUnorderedList () {
+  $('#richTextField').document.execCommand('InsertUnorderedList', false, 'newUL');
+}
+
+function editorOrderedList () {
+  $('#richTextField').document.execCommand('InsertOrderedList', false, 'newOL');
+}
+
+function editorCode () {
+  $('#richTextField').document.execCommand("insertHTML", false, "<pre><code class='editor-code'>"+ document.getSelection()+"</code></pre>");
+}
+
+function sumbitQuestion () {
+  var textArea = $('#hidden-text-area').val()
+  textArea = window.frames['richTextField'].document.body.innerHTML
+  $('#new-question').submit();
+}
+
 $(document).ready(function() {
   iFrameOn();
   sumbitQuestion();
